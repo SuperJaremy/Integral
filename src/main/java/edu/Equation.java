402ed89444fd8a5f8743fb80aa3abdb86e4dcf75
s_jar <legoitcomp@gmail.com>
2021-04-21 20:44:56 +0300

    Добавлены уравнения

--- src/main/java/edu/Equation.java
+++ src/main/java/edu/Equation.java
@@ -3,13 +3,18 @@ package edu;
 import java.util.function.UnaryOperator;
 
 public enum Equation {
-    FIRST("5x^3-2x^2+7x-14",
-            x->5*Math.pow(x,3)-2*Math.pow(x,2)+7*x-14);
-
+    FIRST("5x^3-2x^2+3x-15",
+            x -> 5 * Math.pow(x, 3) - 2 * Math.pow(x, 2) + 3 * x - 15),
+    SECOND("xsin(3x)-23x+14e^(-x)",
+            x -> x * Math.sin(3 * x) - 23 * x + 14 * Math.exp(-x)),
+    THIRD("cos(-15x)*sin(11x)*e^(-3x)",
+            x -> Math.cos(-15 * x) * Math.sin(11 * x) * Math.exp(-3 * x)),
+    FORTH("x^(1/3)*e^(sin(x)*cos(-2*x))",
+            x -> Math.cbrt(x) * Math.exp(Math.sin(x) * Math.cos(-2 * x)));
     private final String name;
     private final UnaryOperator<Double> function;
 
-    Equation(String name, UnaryOperator<Double> function){
+    Equation(String name, UnaryOperator<Double> function) {
         this.name = name;
         this.function = function;
     }
@@ -19,7 +24,7 @@ public enum Equation {
         return name;
     }
 
-    public double getValue(double x){
+    public double getValue(double x) {
         return function.apply(x);
     }
 }
--- src/main/java/edu/io/ConsoleOutput.java
+++ src/main/java/edu/io/ConsoleOutput.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public class ConsoleOutput {
     public void write(OutputData data) {
         BigDecimal epsilon = BigDecimal.valueOf(data.getEpsilon());
-        int precision = epsilon.scale() > 0 ? epsilon.scale() - 1 : 0;
+        int precision = epsilon.scale();
         double result = DoubleRounder.round(data.getSolution(), precision);
         System.out.println("Полученное значение: "+result);
         System.out.println("Количество разбиений: "+data.getStepCount());
